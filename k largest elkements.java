f247019756664a40a4eaa0d9a7c3ec6af2590932
Shivam Singhal <swingle8@gmail.com>
2020-10-17 23:54:21 +0530

    Add files via upload

---
+++ k largest elkements.java
@@ -0,0 +1,53 @@
+//k largest elements
+
+// { Driver Code Starts
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    int [] kLargest(int[] arr, int n, int k) {
+        int [] ans = new int[k];
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for (int i = 0 ; i < arr.length ; i++) {
+            heap.add(arr[i]);
+        }
+        
+        for (int i = 0 ;  i < k ; i++) {
+            ans[i] = heap.remove();
+        }
+        
+        return ans;
+    }
+}
